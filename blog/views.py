ade71d86b9e6af58f22dd89ac4403b4b5cdcdeae
ibibeklamichhane <bibeklc714@gmail.com>
2020-12-03 11:51:30 +0545

    model added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import *
+
+
+def home(request):
+    post = Post.objects.all()
+
+
+    context = {
+        'post': post,
+    }
+    return render(request, 'blog/home.html', context)
